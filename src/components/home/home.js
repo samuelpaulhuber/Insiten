0051103479b64890138eb97520ac924b88af8cdd
red <red@reds-MacBook-Pro.local>
2019-06-13 23:27:02 -0400

    Initial commit

---
+++ src/components/home/home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Alert } from 'react-bootstrap';
+
+function Home() {
+  return (
+    <div className="alert alert-primary">
+        Hello
+    </div>
+  );
+}
+
+export default Home;
