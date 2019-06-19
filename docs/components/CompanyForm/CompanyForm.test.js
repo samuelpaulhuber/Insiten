edb17a8f7f5344eb93c38a8f442aff7b75a78c62
samuelpaulhuber <samhuber84@gmail.com>
2019-06-19 18:26:31 -0400

    test

--- docs/components/CompanyForm/CompanyForm.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import CompanyForm from './CompanyForm';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<CompanyForm />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
--- docs/components/ViewComanies/ViewComanies.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import ViewCompanies from './ViewCompanies';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<ViewCompanies />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
