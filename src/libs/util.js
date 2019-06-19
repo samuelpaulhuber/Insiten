5648ee9ef4e2063e9952b2766001e9c458322808
samuelpaulhuber <samhuber84@gmail.com>
2019-06-18 22:32:39 -0400

    https calls

--- src/libs/util.js
+++ src/libs/util.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const url = 'http://insitenserver.azurewebsites.net/api/'; //'http://localhost:5000/api/'
+const url = 'https://insitenserver.azurewebsites.net/api/'; //'http://localhost:5000/api/'
 const util = {
 	getAll() {
 		axios
